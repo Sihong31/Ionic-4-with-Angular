3bdde4e70f8702ef6317185a979d126902e97de0
Sihong Huang <sihongspring2011@gmail.com>
2019-09-02 11:44:09 -0400

    work on create booking template driven form

--- ionic-angular-bookings/src/app/bookings/create-booking/create-booking.component.ts
+++ ionic-angular-bookings/src/app/bookings/create-booking/create-booking.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Place } from 'src/app/places/place.model';
 import { ModalController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-booking',
@@ -9,21 +10,55 @@ import { ModalController } from '@ionic/angular';
 })
 export class CreateBookingComponent implements OnInit {
   @Input() selectedPlace: Place;
+  @Input() selectedMode: 'select' | 'random';
+  @ViewChild('createBookingForm', {static: true}) form: NgForm;
+  startDate: string;
+  endDate: string;
 
   constructor(private modalCtrl: ModalController) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const availableFrom = new Date(this.selectedPlace.availableFrom);
+    const availableTo = new Date(this.selectedPlace.availableTo);
+    if (this.selectedMode === 'random') {
+      this.startDate = new Date(
+        availableFrom.getTime() + Math.random() * (availableTo.getTime() - 7 * 24 * 60 * 60 * 1000 - availableFrom.getTime())
+      ).toISOString();
+
+      this.endDate = new Date(
+        new Date(this.startDate)
+        .getTime() + Math.random() * (new Date(this.startDate).getTime() + 6 * 24 * 60 * 60 * 1000 - new Date(this.startDate).getTime())
+        ).toISOString();
+    }
+  }
 
   onCancel() {
     this.modalCtrl.dismiss(null, 'cancel');
   }
 
-  onBookPlace() {
+  onSubmit() {
+    if (!this.form.valid || !this.datesValid()) {
+      return;
+    }
+
     this.modalCtrl.dismiss(
       {
-        message: 'This is a dummy message'
+        bookingData: {
+          firstName: this.form.value.firstName,
+          lastName: this.form.value.lastName,
+          guestNumber: this.form.value.guestNumber,
+          startDate: this.form.value.dateFrom,
+          endDate: this.form.value.dateTo
+        }
       },
       'confirm'
     );
+  }
+
+  datesValid() {
+    const startDate = new Date(this.form.value.dateFrom);
+    const endDate = new Date(this.form.value.dateTo);
+
+    return endDate > startDate;
   }
 }
--- ionic-angular-bookings/src/app/places/places.service.ts
+++ ionic-angular-bookings/src/app/places/places.service.ts
@@ -11,19 +11,28 @@ export class PlacesService {
       'Manhattan Mansion',
       'In the heart of central park',
       'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Mansion_de_Lopez_%28Lopez_Heritage_House_or_Nelly%27s_Garden%29.jpg/800px-Mansion_de_Lopez_%28Lopez_Heritage_House_or_Nelly%27s_Garden%29.jpg',
-      200.99),
+      200.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+    ),
     new Place(
       'p2',
       'Disney World',
       'Happiness on earth',
       'https://cdn.pixabay.com/photo/2017/02/14/15/28/walt-disney-world-2066168_960_720.jpg',
-      350.99),
+      350.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+    ),
     new Place(
       'p3',
       'Fancy Palace',
       'Very fancy place to stay',
       'https://cdn.pixabay.com/photo/2017/06/19/15/47/drottningholm-palace-2419776_960_720.jpg',
-      275.50),
+      275.50,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+    ),
   ];
 
   get places() {
